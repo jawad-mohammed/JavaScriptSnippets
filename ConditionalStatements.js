59c5273467cdef91a22793f13ad1c8d2b47ab4c5
Jawad Mohammed <80121274+jawad-mohammed@users.noreply.github.com>
2022-06-16 16:56:38 +0530

    Add files via upload

---
+++ ConditionalStatements.js
@@ -0,0 +1,32 @@
+//switch statements
+// let  role                     /// enter your role??
+// switch (role) {                  
+//     case 'softwareDeveloper':
+//         console.log('You are software developer')
+//         break;
+//      case 'DataScientist' :    
+//      console.log('You are Data scientist');
+//      break;
+//     case 'Graphic Designer':
+//     console.log('YOU ARE GRAPHIC DESIGNER');
+//     break;
+//      default:
+//         console.log('please enter your job role');
+//         break;
+// }
+
+
+// let num =  5
+// switch (true) {
+//   case num > 0:
+//     console.log('Number is positive');
+//     break;
+//   case num == 0:
+//     console.log('Numbers is zero');
+//     break;
+//   case num < 0:
+//     console.log('Number is negative');
+//     break;
+//   default:
+//     console.log('Entered value was not a number');
+// }
