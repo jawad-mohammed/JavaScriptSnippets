4aced52762398d66f676ce68036dbb1adde67ebc
Jawad Mohammed <80121274+jawad-mohammed@users.noreply.github.com>
2022-06-16 16:56:06 +0530

    Add files via upload

---
+++ Loops.js
@@ -0,0 +1,50 @@
+//for loop
+//print 1to10 nums using for loop then add jawad 
+// let user = 'jawad'
+// for(let i=1; i<=10; i++){
+//     console.log(user);
+// }
+
+// const countries = ['Finland', 'Sweden', 'Denmark', 'Norway', 'Iceland']
+// //change in uppercase
+// let newArray=[]
+// for(let i=0;i<countries.length;i++){
+//     newArray.push(countries[i].toUpperCase())
+// }
+
+//create a new Array by looking in given array
+// let arr = [1,2,3,4,5]
+// let newArr = []
+// for(let i=0;i <arr.length;i++){
+//     newArr.push(arr[i]*3)
+// }
+
+// console.log(newArr);
+
+//while loop
+
+// let i=1
+// while( i <5 ){
+//     i++
+// console.log(i);
+// }
+
+//for of loop
+// const nums = [1,2,3,4]
+// for(let num of nums){
+//     console.log(num);
+// }
+
+//break and continue
+
+// for(let i=0;i<=5;i++){
+//     if(i===2){
+//         break
+//     }
+//  console.log(i);
+// }
+// if(i>3){
+//     continue
+//   }
+
+//print if any country with land
